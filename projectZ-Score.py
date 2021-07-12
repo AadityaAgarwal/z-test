e2f7ee8c7a0630309934f7bb58c474bbe1f50f8a
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2021-07-12 18:27:02 +0530

    Update projectZ-Score.py

--- projectZ-Score.py
+++ projectZ-Score.py
@@ -1,8 +1,6 @@
 import csv
 import pandas as pd
 from pandas.io.parsers import read_csv
-# import plotly_express as px
-# import plotly.figure_factory as ff
 import statistics
 import random
 
@@ -10,26 +8,19 @@ df=pd.read_csv('DataSampling.csv')
 
 read_data=list(df['claps'])
 
+dataset=[]
+meanSampleList=[]
 
-meanList=[]
-dataList=[]
-# print(statistics.mean(read_data))
+datamean=statistics.mean(read_data)
 
-def listOfMean():
-
-  dataset=[]
-  for i in range(0,30):
+for i in range(0,100):
+  for x in range(0,30):
     random_index=random.randint(0,(len(read_data)-1))
     value=read_data[random_index]
     dataset.append(value)
-  mean=statistics.mean(dataset)
-  dataList.append(dataset)
-  print(dataset)
-  return mean
+    mean=statistics.mean(dataset)
+  meanSampleList.append(mean)
 
-def finalSetOfData():
-  for i in range(0,100):
-    setOfMean=listOfMean()
-    meanList.append(setOfMean)
-
-print(dataList)
+samplemean=statistics.mean(meanSampleList)
+stdev=statistics.stdev(read_data)
+print(f"z_score= {(datamean-samplemean)/stdev}")
